999a52df16247c8a8beab4c837c16bee6f76ba87
tyagitwishi <tyagitwishi@gmail.com>
2021-03-07 21:07:42 +0530

    updated the adminReview count

--- src/com/twishi/main/models/Constants.java
+++ src/com/twishi/main/models/Constants.java
@@ -3,5 +3,5 @@ package com.twishi.main.models;
 public class Constants {
     public static final Integer criticReviewCount = 3;
     public static final Integer expertReviewCount = 6;
-    public static final Integer adminReviewCount = 6;
+    public static final Integer adminReviewCount = 9;
 }
